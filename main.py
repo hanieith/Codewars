03a611552fb98172d44b7b61e3b2694bdda2b988
Constantin Shkutkov <shkutkoff@gmail.com>
2021-12-02 20:33:00 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# Given a string of words, you need to find the highest scoring word.
+#
+# Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+#
+# You need to return the highest scoring word as a string.
+#
+# If two words score the same, return the word that appears earliest in the original string.
+#
+# All letters will be lowercase and all inputs will be valid.
+# https://www.codewars.com/kata/57eb8fcdf670e99d9b000272
+
+def high(x):
+    lenw = list()
+    x = x.split()
+    words = ' abcdefghijklmnopqrstuvwxyz'
+    c = 0
+    for i in x:
+        if c != 0:
+            lenw.append(c)
+            c = 0
+        for a in i:
+          c += words.find(a)
+    lenw.append(c)
+    z = lenw.index(max(lenw))
+    return x[z]
