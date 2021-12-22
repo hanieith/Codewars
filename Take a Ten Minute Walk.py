65bb62f6ffc2db628c86406f5a206fbf653796e4
Constantin Shkutkov <shkutkoff@gmail.com>
2021-12-22 19:12:47 +0300

    22(1).12

--- Take a Ten Minute Walk.py
+++ Take a Ten Minute Walk.py
@@ -1,6 +1,6 @@
-walk = ['a', 'b', 'c', 'd']
-l, r, s, b = walk
-if count ('l') == count('r') and count('s') == count('b'):
-    return True
-else:
-    return False
+def is_valid_walk(walk):
+    l, r, s, b = walk
+    if count ('l') == count('r') and count('s') == count('b'):
+        return True
+    else:
+        return False
